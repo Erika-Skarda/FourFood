9d4b8cce9395d4a3e6325957b8de41c3aa8e7d2a
Erika-Skarda <erikaskarda@yahoo.com.br>
2020-08-11 21:50:38 -0300

    Dia01-fourfood

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import DarkMode from "./Components/DarkMode/dark-ligth-mode"
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      < DarkMode />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/Containers/Router/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ConnectedRouter } from "connected-react-router";
+import { Switch, Route, Redirect } from "react-router-dom";
+import SplashScreen from "../Pages/SplashScreen/index";
+
+export const routes = {
+    splashScreen: "/",
+    login:"/login",
+    signup:"/signup"
+}
+
+function Router(props: { history: import("history").History<any>; }) {
+    return (
+      <ConnectedRouter history={props.history}>
+        <Switch>
+          <Route exact path={routes.splashScreen} component={SplashScreen} />
+          </Switch>
+      </ConnectedRouter>
+    );
+  }
+  
+  export default Router;
